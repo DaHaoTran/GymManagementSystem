222c55e83977d07c51b47e3cba2cfe9f6e91be98
DaHaoTran <nhao4771@gmail.com>
2025-01-28 22:32:59 +0700

    Add DbContext Folder

---
+++ DBA/Context/GymManagementSystemDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBA.Context
+{
+    public class GymManagementSystemDBContext : DbContext
+    {
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomersVoucher> CustomersVouchers { get; set; }
+        public DbSet<EmployeeSalary> EmployeeSalaries { get; set; }
+        public DbSet<Equipment> Equipment { get; set; }
+        public DbSet<Fine> Fines { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Salary> Salaries { get; set; }
+        public DbSet<ServicePackage> ServicePackages { get; set; }
+        public DbSet<WorkingCheck> WorkingChecks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Code table relationship here ...
+        }
+    }
+}
