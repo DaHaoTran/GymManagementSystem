01cad6b0e2abbfde08af23401bfbd95ce106f3c4
DaHaoTran <nhao4771@gmail.com>
2025-01-28 15:30:04 +0700

    Add Models Library

---
+++ Models/Branch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Branch
+    {
+        [Key]
+        [Column(TypeName = "Varchar(6)")]
+        public string BranchCode { get; set; }
+
+        public string BranchName { get; set; }
+
+        public string Address { get; set; }
+
+        public int QuantityOfStaffs { get; set; }
+    }
+}
