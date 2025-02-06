fdb398d9f5cc3831cf88c10ce0f842e63976b48c
DaHaoTran <nhao4771@gmail.com>
2025-02-06 19:58:03 +0700

    Add migration

---
+++ API/Migrations/20250206125619_GymDB.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class GymDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    AccountCode = table.Column<string>(type: "Varchar(10)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    IdNumber = table.Column<int>(type: "int", nullable: false),
+                    LivingAt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdateBy = table.Column<string>(type: "Varchar(10)", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    SalaryCode = table.Column<string>(type: "Varchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Branches",
+                columns: table => new
+                {
+                    BranchCode = table.Column<string>(type: "Varchar(6)", nullable: false),
+                    BranchName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuantityOfStaffs = table.Column<int>(type: "int", nullable: false),
+                    QuantityOfPTs = table.Column<int>(type: "int", nullable: false),
+                    QuantityOfWorkers = table.Column<int>(type: "int", nullable: false),
+                    AdminUpdate = table.Column<string>(type: "Varchar(10)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Branches", x => x.BranchCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerCode = table.Column<string>(type: "Varchar(12)", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    IsBanned = table.Column<bool>(type: "bit", nullable: false),
+                    BannedReason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BranchCode = table.Column<string>(type: "Varchar(6)", nullable: false),
+                    UpdateBy = table.Column<string>(type: "Varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomersVouchers",
+                columns: table => new
+                {
+                    OrderNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerCode = table.Column<string>(type: "Varchar(12)", nullable: false),
+                    PackageCode = table.Column<string>(type: "Varchar(5)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateBy = table.Column<string>(type: "Varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomersVouchers", x => x.OrderNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeSalaries",
+                columns: table => new
+                {
+                    EmpSalCode = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BranchName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Month = table.Column<DateOnly>(type: "date", nullable: false),
+                    WorkDays = table.Column<int>(type: "int", nullable: false),
+                    PriceTotals = table.Column<decimal>(type: "Money", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false),
+                    AccountCode = table.Column<string>(type: "Varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeSalaries", x => x.EmpSalCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipment",
+                columns: table => new
+                {
+                    EquipCode = table.Column<string>(type: "Varchar(10)", nullable: false),
+                    BranchCode = table.Column<string>(type: "Varchar(6)", nullable: false),
+                    EquipName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StaffUpdate = table.Column<string>(type: "Varchar(10)", nullable: true),
+                    AdminUpdate = table.Column<string>(type: "Varchar(10)", nullable: false),
+                    IsReceived = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipment", x => x.EquipCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fines",
+                columns: table => new
+                {
+                    FineCode = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerCode = table.Column<string>(type: "Varchar(12)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCompensated = table.Column<bool>(type: "bit", nullable: false),
+                    AdminNote = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StaffNote = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdateBy = table.Column<string>(type: "Varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fines", x => x.FineCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    OrderNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.OrderNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salaries",
+                columns: table => new
+                {
+                    SalaryCode = table.Column<string>(type: "Varchar(5)", nullable: false),
+                    SalaryType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PricesApply = table.Column<decimal>(type: "Money", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salaries", x => x.SalaryCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServicePackages",
+                columns: table => new
+                {
+                    PackageCode = table.Column<string>(type: "Varchar(5)", nullable: false),
+                    PackageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemberQuantity = table.Column<int>(type: "int", nullable: false),
+                    NumberOfDays = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    AdminUpdate = table.Column<string>(type: "Varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServicePackages", x => x.PackageCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkingChecks",
+                columns: table => new
+                {
+                    OrderNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CheckDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CheckOf = table.Column<string>(type: "Varchar(10)", nullable: false),
+                    IsCheckIn = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkingChecks", x => x.OrderNumber);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Branches");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "CustomersVouchers");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeSalaries");
+
+            migrationBuilder.DropTable(
+                name: "Equipment");
+
+            migrationBuilder.DropTable(
+                name: "Fines");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Salaries");
+
+            migrationBuilder.DropTable(
+                name: "ServicePackages");
+
+            migrationBuilder.DropTable(
+                name: "WorkingChecks");
+        }
+    }
+}
