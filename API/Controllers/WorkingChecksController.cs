ad531d021f4742ea6dfe2e187e2c215e88809ece
DaHaoTran <nhao4771@gmail.com>
2025-02-20 20:11:49 +0700

    Add woking_check API

---
+++ API/Controllers/WorkingChecksController.cs
@@ -0,0 +1,97 @@
+﻿using API.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace API.Controllers
+{
+    [Route("api/working-checks")]
+    [ApiController]
+    public class WorkingChecksController : ControllerBase
+    {
+        private readonly WorkingCheck_Int _WKSvc;
+        public WorkingChecksController(WorkingCheck_Int wKSvc)
+        {
+            _WKSvc = wKSvc;
+        }
+
+        /// <summary>
+        /// Get working check list
+        /// </summary>
+        /// <returns>working check list</returns>
+        [HttpGet]
+        public async Task<List<WorkingCheck>> GetWorkingCheckList()
+        {
+            return await _WKSvc.GetWorkingCheckList();
+        }
+
+        /// <summary>
+        /// Get the working check by order number
+        /// </summary>
+        /// <param name="order_number">order number</param>
+        /// <returns>a valid working check</returns>
+        [HttpGet("order-number/{order_number}")]
+        public async Task<IActionResult> GetTheWorkingCheckByOrderNumber(int order_number)
+        {
+            if(order_number <= 0) { return BadRequest(); }
+            var getWK = await _WKSvc.GetTheWorkingCheckByOrderNumber(order_number);
+            if(getWK == null) { return NotFound(); }
+            return Ok(getWK);
+        }
+
+        /// <summary>
+        /// Get the working checks by account code
+        /// </summary>
+        /// <param name="account_code">account code</param>
+        /// <returns>valid working check list</returns>
+        [HttpGet("accounts/account-code/{account_code}/working-checks")]
+        public async Task<IActionResult> GetTheWorkingChecksByAccountCode(string account_code)
+        {
+            if(string.IsNullOrEmpty(account_code)) { return BadRequest(); }
+            var getWK = await _WKSvc.GetTheWorkingChecksByAccountCode(account_code);
+            return Ok(getWK);
+        }
+
+        /// <summary>
+        /// Add a new working check
+        /// </summary>
+        /// <param name="workingCheck">working check data</param>
+        /// <returns>working check have added</returns>
+        [HttpPost]
+        public async Task<IActionResult> AddANewWorkingCheck([FromBody] WorkingCheck workingCheck)
+        {
+            if(workingCheck == null) { return BadRequest(); }
+            var newWK = await _WKSvc.AddANewWorkingCheck(workingCheck);
+            return Ok(newWK);
+        }
+
+        /// <summary>
+        /// Edit an exist working check
+        /// </summary>
+        /// <param name="workingCheck">working check data</param>
+        /// <returns>working check have edited</returns>
+        [HttpPut]
+        public async Task<IActionResult> EditAnExistWorkingCheck([FromBody] WorkingCheck workingCheck)
+        {
+            if(workingCheck == null) { return BadRequest(); }
+            var editWK = await _WKSvc.EditAnExistWorkingCheck(workingCheck);
+            if(editWK == null) { return NotFound(); }
+            return Ok(editWK);
+        }
+
+        /// <summary>
+        /// Delete an exist working check
+        /// </summary>
+        /// <param name="order_number">order number</param>
+        /// <returns>working check have deleted</returns>
+        [HttpDelete("{order_number}")]
+        public async Task<IActionResult> DeleteAnExistWorkingCheck(int order_number)
+        {
+            if(order_number <= 0) { return BadRequest(); }
+            var delWK = await _WKSvc.DeleteAnExistWorkingCheck(order_number);
+            if(delWK == null) { return NotFound(); }
+            return Ok(delWK);
+        }
+
+    }
+}
