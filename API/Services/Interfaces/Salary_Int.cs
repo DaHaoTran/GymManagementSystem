c2e9af92a8aecc0068dd5940f3096747f513af6c
DaHaoTran <nhao4771@gmail.com>
2025-02-20 13:54:14 +0700

    Fix salary interface service

--- API/Services/Interfaces/Salary_Int.cs
+++ API/Services/Interfaces/Salary_Int.cs
@@ -4,7 +4,7 @@ namespace API.Services.Interfaces
 {
     public interface Salary_Int
     {
-        List<Salary> GetSalaryList();
+        Task<List<Salary>> GetSalaryList();
         Task<Salary> GetTheSalaryBySalaryCode(string salaryCode);
         Task<Salary> AddANewSalary(Salary salary);
         Task<Salary> EditAnExistSalary(Salary salary);
