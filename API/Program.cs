8fa0399680e997157f7d4cef0db274b9ae4f944f
DaHaoTran <nhao4771@gmail.com>
2025-02-22 10:51:03 +0700

    Add new

--- API/Program.cs
+++ API/Program.cs
@@ -50,6 +50,7 @@ builder.Services.AddScoped<WorkingCheck_Int, WorkingCheck_Imp>();
 builder.Services.AddScoped<EmployeeSalary_Int, EmployeeSalary_Imp>();
 builder.Services.AddScoped<Customer_Int, Customer_Imp>();
 builder.Services.AddScoped<Fine_Int, Fine_Imp>();
+builder.Services.AddScoped<CustomersVoucher_Int, CustomerVoucher_Imp>();
 
 var app = builder.Build();
 
