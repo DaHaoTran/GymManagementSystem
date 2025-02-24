3080d17a6b9b6784ba36cc334ebd6af6c83569c8
DaHaoTran <nhao4771@gmail.com>
2025-02-24 10:29:20 +0700

    Add signalR service

--- Client_FAU/Program.cs
+++ Client_FAU/Program.cs
@@ -1,10 +1,17 @@
 using Client_FAU.Components;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 
 var app = builder.Build();
 
