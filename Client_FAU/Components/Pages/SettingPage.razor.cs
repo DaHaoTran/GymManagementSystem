a8b64c252f8086a565d1408d1ec16e556bd00369
DaHaoTran <nhao4771@gmail.com>
2025-02-27 10:37:04 +0700

    Updating...

---
+++ Client_FAU/Components/Pages/SettingPage.razor.cs
@@ -0,0 +1,36 @@
+﻿using Client_FAU.Business.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Models;
+using Newtonsoft.Json;
+
+namespace Client_FAU.Components.Pages
+{
+    public partial class SettingPage
+    {
+        [Inject]
+        private Salary_Int? SalaryBsn { get; set; }
+        [Inject]
+        private IHttpContextAccessor? HttpContextAccessor { get; set; }
+
+        private List<Salary>? salaries;
+        private string sessionName = "salaries";
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadSalaryList();
+        }
+
+        private async Task LoadSalaryList()
+        {
+            var data = HttpContextAccessor!.HttpContext!.Session.GetString(sessionName);
+            if(data!= null)
+            {
+                salaries = JsonConvert.DeserializeObject<List<Salary>>(data);
+                return;
+            }
+            var getSalaries = await SalaryBsn!.GetSalaryList(12);
+            salaries = getSalaries;
+            HttpContextAccessor!.HttpContext!.Session.SetString(sessionName, JsonConvert.SerializeObject(getSalaries));
+        }
+    }
+}
