5f80c2637882aa3713fdc7d36cb6af4bee2908b7
DaHaoTran <nhao4771@gmail.com>
2025-03-03 11:55:08 +0700

    Add new

---
+++ Client_FAU/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,30 @@
+﻿using Client_FAU.Business.Interfaces;
+using Client_FAU.Variables;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Client_FAU.Components.Layout
+{
+    public partial class MainLayout
+    {
+        [Inject]
+        private Role_Int? RoleBsn { get; set; }
+        [Inject]
+        private HttpContextAccessor? HttpContextAccessor { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetRoleList();
+        }
+
+        private async Task GetRoleList()
+        {
+            var data = HttpContextAccessor!.HttpContext!.Session.GetString(SessionNames.Roles);
+            if(data != null) { return; }
+
+            var roles = await RoleBsn!.GetRoleList(0);
+            HttpContextAccessor!.HttpContext!.Session.SetString(SessionNames.Roles, JsonConvert.SerializeObject(roles));
+        }
+    }
+}
