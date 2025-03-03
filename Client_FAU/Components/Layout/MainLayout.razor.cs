8ecaf4701a1ac2d9c55a22fac83d915f988344ed
DaHaoTran <nhao4771@gmail.com>
2025-03-03 14:38:47 +0700

    Update

--- Client_FAU/Components/Layout/MainLayout.razor.cs
+++ Client_FAU/Components/Layout/MainLayout.razor.cs
@@ -11,11 +11,17 @@ namespace Client_FAU.Components.Layout
         [Inject]
         private Role_Int? RoleBsn { get; set; }
         [Inject]
+        private Salary_Int? SalaryBsn { get; set; }
+        [Inject]
+        private ServicePackage_Int? SPBsn { get; set; }
+        [Inject]
         private IHttpContextAccessor? HttpContextAccessor { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             await GetRoleList();
+            await GetSalaryList();
+            await GetServicePackageList();
         }
 
         private async Task GetRoleList()
@@ -25,6 +31,22 @@ namespace Client_FAU.Components.Layout
 
             var roles = await RoleBsn!.GetRoleList(0);
             HttpContextAccessor!.HttpContext!.Session.SetString(SessionNames.Roles, JsonConvert.SerializeObject(roles));
+        }
+
+        private async Task GetSalaryList()
+        {
+            var data = HttpContextAccessor!.HttpContext!.Session.GetString(SessionNames.Salaries);
+            if (data != null) { return; }
+            var salaries = await SalaryBsn!.GetSalaryList(0);
+            HttpContextAccessor!.HttpContext!.Session.SetString(SessionNames.Salaries, JsonConvert.SerializeObject(salaries));
+        }
+
+        private async Task GetServicePackageList()
+        {
+            var data = HttpContextAccessor!.HttpContext!.Session.GetString(SessionNames.ServicePackages);
+            if (data != null) { return; }
+            var servicePackages = await SPBsn!.GetServicePackageList(0);
+            HttpContextAccessor!.HttpContext!.Session.SetString(SessionNames.ServicePackages, JsonConvert.SerializeObject(servicePackages));
         }
     }
 }
