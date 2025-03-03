56e560d34b7459b5288240798a30a07d7e85d532
DaHaoTran <nhao4771@gmail.com>
2025-03-03 11:55:01 +0700

    Add new

---
+++ Client_FAU/Variables/SessionNames.cs
@@ -0,0 +1,11 @@
+﻿namespace Client_FAU.Variables
+{
+    public static class SessionNames
+    {
+        public static string Roles = "roles";
+
+        public static string Salaries = "salaries";
+
+        public static string ServicePackages = "servicePackages";
+    }
+}
