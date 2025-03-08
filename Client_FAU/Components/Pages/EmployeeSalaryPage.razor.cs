093ac0295dfc861c766727ddfd5ee27daaa72567
DaHaoTran <nhao4771@gmail.com>
2025-03-08 14:12:18 +0700

    Update

---
+++ Client_FAU/Components/Pages/EmployeeSalaryPage.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace Client_FAU.Components.Pages
+{
+    public partial class EmployeeSalaryPage
+    {
+        private int currentYear = DateTime.UtcNow.Year;
+        private int thisYear = DateTime.UtcNow.Year;
+    }
+}
