d6e507e68fc7ad03c73b975a2a8dec7b18b887f1
DaHaoTran <nhao4771@gmail.com>
2025-03-11 15:06:28 +0700

    Add new

---
+++ Client_FAU/Business/Interfaces/Token_Int.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+
+namespace Client_FAU.Business.Interfaces
+{
+    public interface Token_Int
+    {
+        Task<string> GenerateJwtToken(Account account);
+        Task<string> SolveToken(string token);  
+    }
+}
