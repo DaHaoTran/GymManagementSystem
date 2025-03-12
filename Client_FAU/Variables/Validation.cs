a8e40b9eefaae688d2d82b3371ef2cc189d335d5
DaHaoTran <nhao4771@gmail.com>
2025-03-12 11:29:03 +0700

    Add new

---
+++ Client_FAU/Variables/Validation.cs
@@ -0,0 +1,9 @@
+﻿namespace Client_FAU.Variables
+{
+    public static class Validation
+    {
+        public static bool IsLoggedIn { get; set; } = false;
+
+        public static string AccountCode { get; set; } = string.Empty;
+    }
+}
