76205cca60b78b7058b3c618332299a6e9d652ab
DaHaoTran <nhao4771@gmail.com>
2025-03-13 09:49:33 +0700

    Add new

---
+++ Client_FAU/Business/Implements/Jwt_Imp.cs
@@ -0,0 +1,25 @@
+﻿using Client_FAU.Business.Interfaces;
+using Microsoft.JSInterop;
+using System.Net.Http.Headers;
+
+namespace Client_FAU.Business.Implements
+{
+    public class Jwt_Imp : Jwt_Int
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IJSRuntime _jsRunTime;
+        public Jwt_Imp(HttpClient httpClient, IJSRuntime jSRuntime)
+        {
+            _httpClient = httpClient;
+            _jsRunTime = jSRuntime;
+        }
+        public async Task SetAuthorizationHeaderAsync(string localSName)
+        {
+            var token = await _jsRunTime.InvokeAsync<string>("localStorage.getItem", localSName);
+            if (!string.IsNullOrEmpty(token))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+    }
+}
---
+++ Client_FAU/Business/Interfaces/Jwt_Int.cs
@@ -0,0 +1,7 @@
+﻿namespace Client_FAU.Business.Interfaces
+{
+    public interface Jwt_Int
+    {
+        Task SetAuthorizationHeaderAsync(string localSName);
+    }
+}
