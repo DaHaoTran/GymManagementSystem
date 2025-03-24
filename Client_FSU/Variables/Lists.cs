772e5d2232f58cc558ed89ac3af67df906458a89
DaHaoTran <nhao4771@gmail.com>
2025-03-24 11:59:18 +0700

    Add new

---
+++ Client_FSU/Variables/Lists.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+
+namespace Client_FSU.Variables
+{
+    public static class Lists
+    {
+        public static List<Customer> customers = new List<Customer>();
+        public static List<Branch> branches = new List<Branch>();
+    }
+}
